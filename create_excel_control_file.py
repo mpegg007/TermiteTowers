dd435d8a528b7244d00e69ea0a7c4b91b3fe1b73
Matthew Pegg <mpegg@hotmail.com>
2024-10-05 13:40:58 -0400

    adding comment block

--- create_excel_control_file.py
+++ create_excel_control_file.py
@@ -1,3 +1,22 @@
+"""
+% ccm_modify_date: xx2024-10-05 12:38:07 %
+% ccm_author: xxmpegg %
+% ccm_version: xx2 %
+% ccm_repo: xxhttps://github.com/mpegg007/TermiteTowers.git %
+% ccm_branch: xxmain %
+% ccm_object_id: xxsample.py:2 %
+% ccm_commit_id: xx81a1073ed784cee95a9cc17b98f5a6e75dc689f3 %
+% ccm_commit_count: xx2 %
+% ccm_last_commit_message: xxtest2 %
+% ccm_last_commit_author: xxMatthew Pegg %
+% ccm_last_commit_date: xx2024-10-05 12:37:42 -0400 %
+% ccm_file_last_modified: xx2024-10-05 12:37:26 %
+% ccm_file_name: xxsample.py %
+% ccm_file_type: xxtext/x-python %
+% ccm_file_encoding: xxascii %
+% ccm_file_eol: xxCRLF %
+"""
+
 import pandas as pd
 import os
 
