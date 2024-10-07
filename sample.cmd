dbe0012f685a22ac7bd1f5556f48919d210ef70e
Matthew Pegg <mpegg@hotmail.com>
2024-10-07 19:20:10 -0400

    test comment

---
+++ sample.cmd
@@ -0,0 +1,32 @@
+"""
+% ccm_modify_date: 2024-10-05 14:17:39 %
+% ccm_author: mpegg %
+% ccm_version: 8 %
+% ccm_repo: https://github.com/mpegg007/TermiteTowers.git %
+% ccm_branch: main %
+% ccm_object_id: sample.py:8 %
+% ccm_commit_id: dbaa495ea5fbbb2a2f55cea4e3491bace9eec020 %
+% ccm_commit_count: 8 %
+% ccm_last_commit_message: exclude update_keywords.py from hook %
+% ccm_last_commit_author: Matthew Pegg %
+% ccm_last_commit_date: 2024-10-05 13:55:44 -0400 %
+% ccm_file_last_modified: 2024-10-05 14:14:09 %
+% ccm_file_name: sample.py %
+% ccm_file_type: text/x-python %
+% ccm_file_encoding: CRLF %
+% ccm_file_eol: CRLF %
+"""
+# test3
+
+"""
+% date_created: 2024-10-05 14:17:39 %
+% created_by: mpegg %
+% created_by_email: mpegg %
+% version: 8 %
+% git_head: https://github.com/mpegg007/TermiteTowers.git %
+% git_branch: main %
+% full_filespec: sample.py:8 %
+% git_cid: dbaa495ea5fbbb2a2f55cea4e3491bace9eec020 %
+% git_msg: exclude update_keywords.py from hook %
+% filename: sample.py %
+"""
