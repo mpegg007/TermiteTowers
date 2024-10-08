c469486a9ea01c8bafd81ca2abb9c7324c9e0a18
Matthew Pegg <mpegg@hotmail.com>
2024-10-07 20:10:56 -0400

    test pre-commit

--- sample.cmd
+++ sample.cmd
@@ -1,29 +1,29 @@
-:: % ccm_modify_date: 2024-10-05 14:17:39 %
+:: % ccm_modify_date: 2024-10-07 20:10:56 %
 :: % ccm_author: mpegg %
-:: % ccm_version: 8 %
+:: % version: 20 %
 :: % ccm_repo: https://github.com/mpegg007/TermiteTowers.git %
 :: % ccm_branch: main %
-:: % ccm_object_id: sample.py:8 %
+:: % ccm_object_id: sample.cmd:20 %
 :: % ccm_commit_id: dbaa495ea5fbbb2a2f55cea4e3491bace9eec020 %
 :: % ccm_commit_count: 8 %
 :: % ccm_last_commit_message: exclude update_keywords.py from hook %
 :: % ccm_last_commit_author: Matthew Pegg %
-:: % ccm_last_commit_date: 2024-10-05 13:55:44 -0400 %
-:: % ccm_file_last_modified: 2024-10-05 14:14:09 %
-:: % ccm_file_name: sample.py %
-:: % ccm_file_type: text/x-python %
-:: % ccm_file_encoding: CRLF %
+:: % ccm_last_commit_date: 2024-10-07 19:52:23 -0400 %
+:: % ccm_file_last_modified: 2024-10-07 20:10:41 %
+:: % ccm_file_name: sample.cmd %
+:: % ccm_file_type: text/plain %
+:: % ccm_file_encoding: us-ascii %
 :: % ccm_file_eol: CRLF %
 
 :: test9
 
-:: %date_created: 2024-10-07 19:52:23 %
-:: %created_by: Matthew Pegg %
-:: %created_by_email: mpegg@hotmail.com %
-:: %version: 19 %
-:: %git_head: https://github.com/mpegg007/TermiteTowers.git %
-:: %git_branch: main %
-:: %full_filespec: sample.cmd:19 %
-:: %git_cid: 3578d9048ff4247adfa153c58fb96d0006c57c65 %
-:: %git_msg: test %
-:: %filename: sample.cmd %
+:: % date_created: 2024-10-07 19:52:23 %
+:: % created_by: Matthew Pegg %
+:: % created_by_email: mpegg@hotmail.com %
+:: % version: 19 %
+:: % git_head: https://github.com/mpegg007/TermiteTowers.git %
+:: % git_branch: main %
+:: % full_filespec: sample.cmd:19 %
+:: % git_cid: d8efb9faaeaf168f4a278487fc3a8543a88b2c82 %
+:: % git_msg: test %
+:: % filename: sample.cmd %
