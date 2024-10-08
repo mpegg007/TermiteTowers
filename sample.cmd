16c7536b7f720b4fa8a6ef398ce2f9af9d9087a0
Matthew Pegg <mpegg@hotmail.com>
2024-10-07 20:42:24 -0400

    test

--- sample.cmd
+++ sample.cmd
@@ -1,15 +1,15 @@
-:: % ccm_modify_date: 2024-10-07 20:37:47 %
+:: % ccm_modify_date: 2024-10-07 20:42:24 %
 :: % ccm_author: mpegg %
 :: % version: 20 %
 :: % ccm_repo: https://github.com/mpegg007/TermiteTowers.git %
 :: % ccm_branch: main %
-:: % ccm_object_id: sample.cmd:22 %
-:: % ccm_commit_id: 2de360a7adf9bc84f9ba33147326031d799f918c %
-:: % ccm_commit_count: 22 %
-:: % ccm_last_commit_message: pre-commit hooks %
+:: % ccm_object_id: sample.cmd:23 %
+:: % ccm_commit_id: 4e7e33ef0231f49ddd26414b124e4adc793e6db7 %
+:: % ccm_commit_count: 23 %
+:: % ccm_last_commit_message: test other file types %
 :: % ccm_last_commit_author: Matthew Pegg %
-:: % ccm_last_commit_date: 2024-10-07 20:32:08 -0400 %
-:: % ccm_file_last_modified: 2024-10-07 20:36:54 %
+:: % ccm_last_commit_date: 2024-10-07 20:37:47 -0400 %
+:: % ccm_file_last_modified: 2024-10-07 20:42:14 %
 :: % ccm_file_name: sample.cmd %
 :: % ccm_file_type: text/plain %
 :: % ccm_file_encoding: us-ascii %
@@ -17,30 +17,4 @@
 
 :: test15
 
-    sed -i "s|% ccm_repo: https://github.com/mpegg007/TermiteTowers.git %|g" "$FILE"
-    sed -i "s|% ccm_branch: main %|g" "$FILE"
-    sed -i "s|% ccm_object_id: sample.cmd:22 %|g" "$FILE" 
-    sed -i "s|% ccm_last_commit_author: Matthew Pegg %|g" "$FILE"
-    sed -i "s|% ccm_last_commit_email: mpegg@hotmail.com %|g" "$FILE"
-    sed -i "s|% ccm_commit_id: 2de360a7adf9bc84f9ba33147326031d799f918c %|g" "$FILE"
-    sed -i "s|% ccm_last_commit_message: pre-commit hooks %|g" "$FILE"
-    sed -i "s|% ccm_file_name: sample.cmd %|g" "$FILE"
-    sed -i "s|% ccm_modify_date: 2024-10-07 20:37:47 %|g" "$FILE"
-    sed -i "s|% ccm_version: 22 %|g" "$FILE"
-    sed -i "s|% ccm_last_commit_date: 2024-10-07 20:32:08 -0400 %|g" "$FILE"
-    sed -i "s|% ccm_file_last_modified: 2024-10-07 20:36:54 %|g" "$FILE"
-    sed -i "s|% ccm_file_type: text/plain %|g" "$FILE"
-    sed -i "s|% ccm_file_encoding: us-ascii %|g" "$FILE"
-    sed -i "s|% ccm_file_eol: CRLF %|g" "$FILE"
 
-
-:: % date_created: 2024-10-07 19:52:23 %
-:: % created_by: Matthew Pegg %
-:: % created_by_email: mpegg@hotmail.com %
-:: % version: 19 %
-:: % git_head: https://github.com/mpegg007/TermiteTowers.git %
-:: % git_branch: main %
-:: % full_filespec: sample.cmd:19 %
-:: % git_cid: d8efb9faaeaf168f4a278487fc3a8543a88b2c82 %
-:: % git_msg: test %
-:: % filename: sample.cmd %
