563104db1ae5765a13f067105bacd82f2b224651
Schema Tester <schema@test>
2025-08-29 07:29:39 -0400

    chore(ccm): adopt streamlined header schema

--- ccm_hook_test.sh
+++ ccm_hook_test.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-# % ccm_modify_date: 2025-08-29 07:29:23 %
+# % ccm_modify_date: 2025-08-29 07:29:39 %
 # % ccm_author: Schema Tester %
 # % ccm_author_email: schema@test %
 # % ccm_repo: https://github.com/mpegg007/TermiteTowers.git %
 # % ccm_branch: main %
-# % ccm_object_id: ccm_hook_test.sh:52 %
-# % ccm_commit_id: c39437f3118f4b8058f89b7c7cffb51a551f0b60 %
-# % ccm_commit_count: 52 %
+# % ccm_object_id: ccm_hook_test.sh:53 %
+# % ccm_commit_id: 51b600520f6444716e7cc428637177bbd1f44bae %
+# % ccm_commit_count: 53 %
 # % ccm_commit_message: chore(ccm): adopt streamlined header schema %
 # % ccm_commit_author: Schema Tester %
 # % ccm_commit_email: schema@test %
-# % ccm_commit_date: 2025-08-29 07:29:23 -0400 %
-# % ccm_file_last_modified: 2025-08-29 07:29:23 %
+# % ccm_commit_date: 2025-08-29 07:29:39 -0400 %
+# % ccm_file_last_modified: 2025-08-29 07:29:39 %
 # % ccm_file_name: ccm_hook_test.sh %
 # % ccm_file_type: text/x-shellscript %
 # % ccm_file_encoding: us-ascii %
 # % ccm_file_eol: CRLF %
 # % ccm_path: ccm_hook_test.sh %
-# % ccm_blob_sha: 6458c1393a4bb97d169e14c06d25395e16a2d58e %
+# % ccm_blob_sha: ed345ecedafd4da4580384c8c229e336faf1b782 %
 # % ccm_exec: no %
-# % ccm_size: 941 %
+# % ccm_size: 1063 %
 
 # Simple body to verify the hook doesn't alter runtime behavior
 printf "CCM hook test script running...\n"
@@ -28,3 +28,5 @@ printf "CCM hook test script running...\n"
 # smoke test line 1756466199
 
 # schema smoke test 1756466963
+
+# schema smoke test 1756466979
